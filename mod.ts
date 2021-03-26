b6ad9e1fc82cb57b0052e9a4700fd27b85b157cb
Gnlow <entrydark@gmail.com>
2021-03-26 10:35:27 +0900

    log

--- mod.ts
+++ mod.ts
@@ -1,6 +1,7 @@
 import { serve } from "https://deno.land/std@0.90.0/http/server.ts"
 
 const server = serve({ port: 8000 })
+console.log("Corsi - port: 8000")
 
 for await (const req of server) {
     const data = await fetch(req.url.substring(1)).then(res => res.arrayBuffer())
