119d69e7834aa42daf3eed31d1d9c4c636d4204f
Gnlow <entrydark@gmail.com>
2021-03-26 10:17:21 +0900

    Create mod.ts

---
+++ mod.ts
@@ -0,0 +1,16 @@
+import { serve } from "https://deno.land/std@0.90.0/http/server.ts"
+
+const server = serve({ port: 8000 })
+
+for await (const req of server) {
+    const data = await fetch(req.url.substring(1)).then(res => res.arrayBuffer())
+    const body = new Uint8Array(data)
+
+    req.respond({
+        status: 200,
+        body,
+        headers: new Headers({
+            "Access-Control-Allow-Origin": "*"
+        })
+    })
+}
